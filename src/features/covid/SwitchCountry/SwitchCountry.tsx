473defe80ef94ab23a694df9bff556bc961ca8c4
esakat <esaka.tom@gmail.com>
2020-12-23 18:29:34 +0900

    finish that

---
+++ src/features/covid/SwitchCountry/SwitchCountry.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { NativeSelect, FormControl } from "@material-ui/core";
+import { Doughnut } from "react-chartjs-2";
+
+import { useDispatch } from "react-redux";
+import { fetchAsyncGetCountry } from "../covidSlice";
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    marginBottom: theme.spacing(3),
+    minWidth: 320,
+  },
+}));
+
+const SwitchCountry: React.FC = () => {
+  const classies = useStyles();
+  const dispatch = useDispatch();
+
+  const countries = [
+    "japan",
+    "china",
+    "us",
+    "france",
+    "italy",
+    "spain",
+    "united kingdom",
+    "germany",
+    "russia",
+    "brazil",
+    "taiwan",
+    "thailand",
+    "new zealand",
+    "sweden",
+    "india",
+  ];
+
+  return (
+    <FormControl className={classies.formControl}>
+      <NativeSelect
+        onChange={(e: React.ChangeEvent<HTMLSelectElement>) =>
+          dispatch(fetchAsyncGetCountry(e.target.value))
+        }
+      >
+        <option value="">WorldWide</option>
+        {countries.map((country, i) => (
+          <option key={i} value={country}>
+            {country}
+          </option>
+        ))}
+      </NativeSelect>
+    </FormControl>
+  );
+};
+
+export default SwitchCountry;
